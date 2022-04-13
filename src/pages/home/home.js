38bd80c6f8589778c713aed5395198420cdaac22
Huong <19110084@student.hcmute.edu.vn>
2022-04-13 12:19:08 +0700

    home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 
 export default function Home() {
-    return (
+  return (
+    <div className="home__container">
+      <div className="image"></div>
+      <div className="text">
         <h1>Home</h1>
-    )
+        <h2>Online Test</h2>
+        <h3>BlackPink</h3>
+        <h4>BlackPink in your area</h4>
+      </div>
+    </div>
+  );
 }
